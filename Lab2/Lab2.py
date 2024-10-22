ec69d511d171a943c947b23935f2528f3e261885
EMERY <EMERY.23@ichat.sp.edu.sg>
2024-10-22 10:06:18 +0800

    Refactor Lab2.py to add new functions and update main function

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -1,1 +1,30 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("display_main_menu")
+
+def get_user_input():
+    print("get_user_input")
+    return []
+
+def calc_average(num_list):
+    print("calc_average")
+    return 0.0
+
+def find_min_max(num_list):
+    print("find_min_max")
+    return [0.0, 0.0]
+
+def sort_temperature(num_list):
+    print("sort_temperature")
+    return []
+
+def calc_median_temperature(num_list):
+    print("calc_median_temperature")
+    return 0.0
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+
+if __name__ == "__main__":
+    main()
