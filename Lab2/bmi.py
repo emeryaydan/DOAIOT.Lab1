4dbf1ed57b07e5dd2f0600a33af85943f193be2e
EMERY <EMERY.23@ichat.sp.edu.sg>
2024-10-22 09:54:59 +0800

    Refactor calculate_bmi function in Lab2.py

--- Lab2/bmi.py
+++ Lab2/bmi.py
@@ -1,4 +1,8 @@
 def calculate_bmi(height, weight):
     print("Height = " + str(height))
     print("Weight = " + str(weight))
+    bmi = weight / (height * height)
+    print("BMI = " + str(round(bmi, 2)))
+    return bmi
+
 calculate_bmi(weight=57, height=1.73)
