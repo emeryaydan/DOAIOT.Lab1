8d91ba76ed6f08a10a385bbeed26195ae38a1133
EMERY <EMERY.23@ichat.sp.edu.sg>
2024-10-22 10:08:50 +0800

    Refactor get_user_input function in Lab2.py to accept user input as a comma-separated list of numbers

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -1,9 +1,11 @@
 def display_main_menu():
-    print("display_main_menu")
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
 
 def get_user_input():
-    print("get_user_input")
-    return []
+    input_string = input()
+    string_list = input_string.split(",")
+    float_list = [float(num) for num in string_list]
+    return float_list
 
 def calc_average(num_list):
     print("calc_average")
@@ -25,6 +27,7 @@ def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
     display_main_menu()
     num_list = get_user_input()
+    print(num_list)
 
 if __name__ == "__main__":
     main()
