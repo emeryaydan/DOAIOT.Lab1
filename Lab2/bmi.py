5635478347b4ec8c29f33b0c22457eeb522525e0
EMERY <EMERY.23@ichat.sp.edu.sg>
2024-10-22 09:54:04 +0800

    Refactor calculate_bmi function in Lab2.py
    Update the calculate_bmi function in Lab2.py to properly print the height and weight values. Convert the height and weight variables to strings before concatenating them with the print statement.

--- Lab2/bmi.py
+++ Lab2/bmi.py
@@ -1,4 +1,4 @@
 def calculate_bmi(height, weight):
-    print("Height = " + height)
-    print("Weight = " + weight)
-calculate_bmi(weight="57", height="1.73")
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+calculate_bmi(weight=57, height=1.73)
