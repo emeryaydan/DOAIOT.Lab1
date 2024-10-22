927907f64d4d9bcd27f79753ac0e123b4b29c53b
EMERY <EMERY.23@ichat.sp.edu.sg>
2024-10-22 09:56:39 +0800

    Refactor calculate_bmi function in Lab2.py

--- Lab2/bmi.py
+++ Lab2/bmi.py
@@ -3,6 +3,15 @@ def calculate_bmi(height, weight):
     print("Weight = " + str(weight))
     bmi = weight / (height * height)
     print("BMI = " + str(round(bmi, 2)))
+    
+    if bmi < 18.5:
+        classification = "Under Weight"
+    elif 18.5 <= bmi <= 25.0:
+        classification = "Normal Weight"
+    else:
+        classification = "Over Weight"
+    
+    print("Weight Classification: " + classification)
     return bmi
 
 calculate_bmi(weight=57, height=1.73)
